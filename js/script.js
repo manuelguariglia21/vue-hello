e975170af007b7bfa388a28ac2c0abaa142b1d7a
Manuel Guariglia <manuelguariglia@gmail.com>
2021-11-10 15:30:51 +0100

    fix style

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello World!',
+      name: 'Manuel!',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlTq41YW_RQReBCLYBRs4vBQSuo0tY1Us6Yw&usqp=CAU'
     }
   });
