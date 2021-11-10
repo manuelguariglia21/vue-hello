6cff14e3c37fd5cbc75d3810990ccb1c10194d7a
Manuel Guariglia <manuelguariglia@gmail.com>
2021-11-10 15:22:59 +0100

    fix image

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello World!'
+      message: 'Hello World!',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlTq41YW_RQReBCLYBRs4vBQSuo0tY1Us6Yw&usqp=CAU'
     }
   });
